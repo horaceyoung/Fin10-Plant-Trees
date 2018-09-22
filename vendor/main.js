bb5cf24b073907fae00a1c7f940fc2a76d0df7b3
Lyu Zizheng <31028570+Brabalawuka@users.noreply.github.com>
2018-09-22 21:52:43 +0800

    Add files via upload

---
+++ vendor/main.js
@@ -0,0 +1,39 @@
+function showpopup1() {
+  var darktree = document.getElementById('tree1-1');
+  darktree.style.visibility = "hidden";
+}
+
+function showpopup2() {
+  var darktree = document.getElementById('tree2-1');
+  darktree.style.visibility = "hidden";
+}
+
+function showpopup3() {
+  var darktree = document.getElementById('tree3-1');
+  darktree.style.visibility = "hidden";
+}
+
+function showpopup4() {
+  var darktree = document.getElementById('tree4-1');
+  darktree.style.visibility = "hidden";
+}
+
+function hidepopup1() {
+  var darktree = document.getElementById('tree1-1');
+  darktree.style.visibility = "visible";
+}
+
+function hidepopup2() {
+  var darktree = document.getElementById('tree2-1');
+  darktree.style.visibility = "visible";
+}
+
+function hidepopup3() {
+  var darktree = document.getElementById('tree3-1');
+  darktree.style.visibility = "visible";
+}
+
+function hidepopup4() {
+  var darktree = document.getElementById('tree4-1');
+  darktree.style.visibility = "visible";
+}
